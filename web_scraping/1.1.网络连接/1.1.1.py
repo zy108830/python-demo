07a21b76334d2d249e35c58cfde79ef80c8e25ee
mypc <924714558@qq.com>
2016-11-22 14:11:44 +0800

    测试

--- web_scraping/1.1.网络连接/1.1.1.py
+++ web_scraping/1.1.网络连接/1.1.1.py
@@ -1,4 +1,7 @@
 # 它查找Python的request模块,但是只导入一个urlopen函数
 from urllib.request import urlopen
-html = urlopen("http://www.siguoya.name/")
-print(html.read())
+for x in range(10):
+    try:
+        html = urlopen("http://www.siguoya.name/" + str(x))
+    except BaseException:
+        print(x)
