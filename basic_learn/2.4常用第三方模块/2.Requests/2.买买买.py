93829c02f5bf39e04d3218dfb8378c55dfaee269
siguoya <924714558@qq.com>
2017-02-10 14:29:50 +0800

    优化商品的价格查询方式

--- basic_learn/2.4常用第三方模块/2.Requests/2.买买买.py
+++ basic_learn/2.4常用第三方模块/2.Requests/2.买买买.py
@@ -1,44 +1,42 @@
-import requests, json,sys,time
+import requests, json, sys, time
 from bs4 import BeautifulSoup
 
 file = open(sys.path[0] + '/config.json', 'r')
 config = json.loads(file.read())
 
+
 class Buy(object):
     def __init__(self):
         self.price = {
-            'paw': '',
             'mobileSSD': '',
-            '1pwd':''
+            '1pwd': ''
         }
 
     def get_paw_price(self):
         res = requests.get('https://paw.cloud/')
         soup = BeautifulSoup(res.text, 'lxml')
         labels = soup.select('.btn-purchase-label')
-        self.price['paw'] = str.replace(str.strip(labels[1].text),'Buy Paw for ','')
+        self.price['paw'] = str.replace(str.strip(labels[1].text), 'Buy Paw for ', '')
 
     def get_samsung_ssd_price(self):
         res = requests.get(
-            'https://p.3.cn/prices/get?type=1&area=19_1601_3634&pdtk=&pduid=349952081&pdpin=245165558-998015&pdbp=0&skuid=J_2376036',
+            'http://s.manmanbuy.com/Default.aspx?smallclass=0&ppid=0&siteid=&searchmode=0&f1=0&f2=0&f3=0&f4=0&f5=0&f6=0&price1=0&price2=0&orderby=score&iszy=1&istmall=0&key=MU-PT500B%2fCN',
             headers={
-                'HOST': 'p.3.cn',
-                'Referer': 'https://search.jd.com/Search?keyword=%E7%A7%BB%E5%8A%A8ssd&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&wq=%E7%A7%BB%E5%8A%A8ssd&psort=3&click=0',
+                'HOST': 's.manmanbuy.com',
+                'Referer': 'http://s.manmanbuy.com/Default.aspx?key=MU-PT500B%2FCN&btnSearch=%CB%D1%CB%F7',
                 'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
             })
-        goods = json.loads(res.text)
-        try:
-            #m是厂家指导价
-            info = '三星移动SSD的京东原价是' + goods[0]['op']+'，秒杀价是'+goods[0]['p']
-            self.price['mobileSSD'] = info
-        except:
-            self.send_error('京东价格数据解析出错：'+res.text)
-
+        list = BeautifulSoup(res.text, 'lxml').select('.bjlineSmall')
+        priceInfo = '三星移动固态硬盘: '
+        for item in list:
+            priceInfo += '【' + item.select('.f14proname')[0].text + item.select('.listpricespan')[0].text + '】' + \
+                         item.select('.priceyouhui')[0].text + '；'
+        self.price['mobileSSD'] = priceInfo
 
     def get_1pwd_price(self):
-        res=requests.get('https://itunes.apple.com/us/app/1password-password-manager/id568903335?mt=8')
-        soup=BeautifulSoup(res.text,'lxml')
-        self.price['1pwd']=soup.select('.in-app-price')[0].text
+        res = requests.get('https://itunes.apple.com/us/app/1password-password-manager/id443987910?mt=12&ign-mpt=uo=2')
+        soup = BeautifulSoup(res.text, 'lxml')
+        self.price['1pwd'] = soup.select('.price')[0].text
 
     def send_notioce(self):
         requests.post('https://hook.bearychat.com/=bw8S2/incoming/' + config['bearychat']['token'], data={
@@ -49,13 +47,13 @@ class Buy(object):
                 "attachments": [
                     {
                         "title": "价格监控",
-                        "text": json.dumps(self.price,ensure_ascii=False)
+                        "text": json.dumps(self.price, ensure_ascii=False)
                     }
                 ]
             })
         })
 
-    def send_error(self,error_msg):
+    def send_error(self, error_msg):
         requests.post('https://hook.bearychat.com/=bw8S2/incoming/' + config['bearychat']['token'], data={
             'payload': json.dumps({
                 "text": "买买买 - " + time.strftime('%Y-%m-%d %X', time.localtime()),
@@ -70,8 +68,8 @@ class Buy(object):
             })
         })
 
+
 buy = Buy()
-buy.get_paw_price()
 buy.get_samsung_ssd_price()
 buy.get_1pwd_price()
 buy.send_notioce()
