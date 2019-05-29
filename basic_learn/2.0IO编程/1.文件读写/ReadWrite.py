891dcb06aef09e3f9dde697b68180e82db26e21d
siguoya <924714558@qq.com>
2019-05-29 15:09:56 +0800

    获取上百G的文件的行数

--- basic_learn/2.0IO编程/1.文件读写/ReadWrite.py
+++ basic_learn/2.0IO编程/1.文件读写/ReadWrite.py
@@ -1,20 +1,8 @@
-# 将文件内容转换为map格式
-# 例如将
-# 1
-# 2
-# 3
-# 转换为
-# 1 a
-# 2 b
-# 3 c
-
-f = open('/Users/xinchao/qiniu_migrate/migrate_headportrait.list2.txt')
-
-fileList = []
-
-for line in f.readlines():
-    fileList.append(str.strip(line) + "\t" + "headportrait-bucket/"+line)
-
-with open('/Users/xinchao/qiniu_migrate/migrate_headportrait.list3.txt', 'w') as f:
-    for fileName in fileList:
-        f.write(fileName)
+# 获取上百G的文件的行数
+line_count = 0
+with open('/path/to/file', 'r') as f:
+    for line in f:
+        line_count += 1
+        if line_count % 1000 == 0:
+            print(line_count)
+    print(line_count)
